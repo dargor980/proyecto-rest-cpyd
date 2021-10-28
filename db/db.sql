f86c06ccbcf8c7b2bbe28d4eef5c372eb3c9c96a
dargor980 <german.contrerasa@utem.cl>
2021-10-27 22:24:39 -0300

    bd modelada

--- db/db.sql
+++ db/db.sql
@@ -4,3 +4,34 @@ CREATE TABLE IF NOT EXISTS users(
     email VARCHAR NOT NULL,
     token VARCHAR NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS stations(
+    id SERIAL PRIMARY KEY NOT NULL,
+    nombre VARCHAR NOT NULL,
+    latitud FLOAT NOT NULL,
+    longitud FLOAT NOT NULL,
+    altura FLOAT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS fecha(
+    id SERIAL KEY PRIMARY KEY NOT NULL,
+    mes VARCHAR NOT NULL,
+    dia VARCHAR NOT NULL,
+    anio VARCHAR NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS registers(
+    id SERIAL PRIMARY KEY NOT NULL,
+    temp_maxima FLOAT NOT NULL,
+    temp_minima FLOAT NOT NULL,
+    precipitacion FLOAT NOT NULL,
+    id_station INT,
+    id_fecha INT,
+    CONSTRAINT fk_station
+        FOREIGN KEY(id_station)
+            REFERENCES stations(id)
+    CONSTRAINT fk_fecha
+        FOREIGN KEY(id_fecha)
+            REFERENCES fecha(id)
+);
