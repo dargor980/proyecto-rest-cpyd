49662d4fb4075a14caa15b443f03d45bc0cb037b
Benjamin Cristi <benjamin.cristis@utem.cl>
2021-10-30 23:57:04 -0300

    ADD: MIDDLEWARE

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -2,17 +2,17 @@ const { Router } = require('express');
 const express = require('express');
 const app = express();
 const router = Router();
-const middleware = require('./middleware/auth');
+const middleware = require('../middleware/auth');
 
 const { loginClient, getStations, getStation, search, getEstimate, populateDb, createUser} = require('../controllers/globalController');
 
 router.post('/grupo-E/login', loginClient);
 router.post('/grupo-E/createUser/',createUser);
 router.get('/grupo-E/stations',middleware.checkToken, getStations);
-router.get('/grupo-E/:codigo/stations', getStation);
-router.post('/grupo-E/search', search);
-router.get('/grupo-E/:indicador/:latitud/:longitud/estimate', getEstimate);
-router.get('/grupo-E/populate', populateDb);
+router.get('/grupo-E/:codigo/stations',middleware.checkToken, getStation);
+router.post('/grupo-E/search',middleware.checkToken, search);
+router.get('/grupo-E/:indicador/:latitud/:longitud/estimate',middleware.checkToken, getEstimate);
+router.get('/grupo-E/populate',middleware.checkToken, populateDb);
 
 app.use('/', router);
 
